5c831184c60753e97b365a6a9a21fa59d891164e
qwq140 <qwq140@gmail.com>
2020-12-24 17:28:41 +0900

    20.12.24 경고창 한글깨지는 현상 해결

--- src/com/cos/hello/util/Script.java
+++ src/com/cos/hello/util/Script.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 public class Script {
 	
 	public static void back(HttpServletResponse resp, String msg) throws IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		
 		PrintWriter out = resp.getWriter();
 		out.println("<script>");
 		out.println("alert('"+msg+"');");
@@ -17,6 +20,9 @@ public class Script {
 	}
 	
 	public static void href(HttpServletResponse resp, String url, String msg) throws IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		
 		PrintWriter out = resp.getWriter();
 		out.println("<script>");
 		out.println("alert('"+msg+"');");
